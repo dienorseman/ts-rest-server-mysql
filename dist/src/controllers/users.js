f40072c440f4abb8832eee0739c96a3d8764e90d
dienorseman <edgar.elps@gmail.com>
2023-09-30 15:17:33 -0600

    src folder

---
+++ dist/src/controllers/users.js
@@ -0,0 +1,66 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
+const user_1 = __importDefault(require("../models/user"));
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield user_1.default.findAll();
+    res.json({ users });
+});
+exports.getUsers = getUsers;
+const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const user = yield user_1.default.findByPk(id);
+    if (user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).json({
+            msg: `User with id ${id} not found`
+        });
+    }
+});
+exports.getUser = getUser;
+const postUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { body } = req;
+    const user = user_1.default.build(body);
+    yield user.save();
+    res.json({ user });
+});
+exports.postUser = postUser;
+const putUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const { body } = req;
+    const user = yield user_1.default.findByPk(id);
+    if (!user) {
+        return res.status(404).json({
+            msg: `User with id ${id} not found`
+        });
+    }
+    yield user.update(body);
+    res.json(user);
+});
+exports.putUser = putUser;
+const deleteUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const user = yield user_1.default.findByPk(id);
+    if (!user) {
+        return res.status(404).json({
+            msg: `User with id ${id} not found`
+        });
+    }
+    yield user.update({ status: 0 });
+    res.json(user);
+});
+exports.deleteUser = deleteUser;
